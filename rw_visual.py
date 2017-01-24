c13fbac68365dd02e283792bfc0fc88775ae1745
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-24 15:49:19 -0600

    Generating Multiple Random Walks

--- rw_visual.py
+++ rw_visual.py
@@ -2,9 +2,16 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# make a random walk, and plot the points
-rw = RandomWalk()
-rw.fill_walk()
+# keep making new walks, as long as the program is active
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+while True:
+  # make a random walk, and plot the points
+  rw = RandomWalk()
+  rw.fill_walk()
+
+  plt.scatter(rw.x_values, rw.y_values, s=15)
+  plt.show()
+
+  keep_running = input("Make another walk? (y/n): ")
+  if keep_running == "n":
+    break
