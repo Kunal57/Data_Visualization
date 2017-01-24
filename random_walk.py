25efae105f3be31cc01f28e8a76d6c286fea94de
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-24 16:23:07 -0600

    Refactor fill_walk()

--- random_walk.py
+++ random_walk.py
@@ -17,13 +17,8 @@ class RandomWalk():
     # Keep taking steps until the walk reaches the desired length.
     while len(self.x_values) < self.num_points:
       # Decide which direction to go and how far to go in the direction
-      x_direction = choice([1, -1])
-      x_distance = choice([0, 1, 2, 3, 4])
-      x_step = x_direction * x_distance
-
-      y_direction = choice([1, -1])
-      y_distance = choice([0, 1, 2, 3, 4])
-      y_step = y_direction * y_distance
+      x_step = self.get_step()
+      y_step = self.get_step()
 
       # reject moves that go nowhere
       if x_step == 0 and y_step == 0:
@@ -35,3 +30,7 @@ class RandomWalk():
 
       self.x_values.append(next_x)
       self.y_values.append(next_y)
+
+  def get_step(self):
+    """ Decide which direction to go and how far to go in the direction """
+    return choice([1, -1]) * choice([0, 1, 2, 3, 4])
