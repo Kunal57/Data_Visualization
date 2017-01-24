68e85e4a2ac1e4cc3fe5d08aa51b2f2af5bde11c
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-24 14:55:36 -0600

    Removing Outlines from Data Points

--- scatter_squares.py
+++ scatter_squares.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=40)
+plt.scatter(x_values, y_values, edgecolor="none", s=40)
 
 # set chart title and label axes
 plt.title("Square Numbers", fontsize=24)
