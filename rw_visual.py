0bd3bbf599836feae20c049f3cd6efc61076ecc2
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-24 15:54:57 -0600

    Coloring the Points

--- rw_visual.py
+++ rw_visual.py
@@ -9,7 +9,8 @@ while True:
   rw = RandomWalk()
   rw.fill_walk()
 
-  plt.scatter(rw.x_values, rw.y_values, s=15)
+  point_numbers = list(range(rw.num_points))
+  plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor="none", s=15)
   plt.show()
 
   keep_running = input("Make another walk? (y/n): ")
