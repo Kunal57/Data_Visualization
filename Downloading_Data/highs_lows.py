3bbd5c31afc781570528bc569f64d8b6e5726802
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-26 15:23:10 -0600

    Plotting Data in a Temperature Chart

--- Downloading_Data/highs_lows.py
+++ Downloading_Data/highs_lows.py
@@ -1,5 +1,7 @@
 import csv
 
+from matplotlib import pyplot as plt
+
 # get high temperatures from file
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
@@ -11,5 +13,14 @@ with open(filename) as f:
     high = int(row[1])
     highs.append(high)
 
+# plot data
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(highs, c="red")
 
-  print(highs)
+# format plot
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.xlabel("", fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis="both", which="major", labelsize=16)
+
+plt.show()
