d59a111d3e74d33fa70f4c3fe17ed9af17f45f91
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-26 15:39:58 -0600

    Shading an Area in the Chart

--- Downloading_Data/highs_lows.py
+++ Downloading_Data/highs_lows.py
@@ -22,12 +22,13 @@ with open(filename) as f:
 
 # plot data
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c="red")
-plt.plot(dates, lows, c="blue")
+plt.plot(dates, highs, c="red", alpha=0.5)
+plt.plot(dates, lows, c="blue", alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
 
 # format plot
-plt.title("Daily high temperatures - 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel("", fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
