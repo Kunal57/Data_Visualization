741d93458e906133d10309f3678835657c554999
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-26 18:53:06 -0600

    Summarizing the Top Respositories

--- Working_With_APIs/python_repos.py
+++ Working_With_APIs/python_repos.py
@@ -12,14 +12,10 @@ print("Total repositories:", response_dict['total_count'])
 # explore information about the repositories
 repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
-
-# examine the first repository
-repo_dict = repo_dicts[0]
-print("\nSelected information about first repository:")
-print('Name:', repo_dict['name'])
-print('Owner', repo_dict['owner']['login'])
-print('Stars:', repo_dict['stargazers_count'])
-print('Repository:', repo_dict['html_url'])
-print('Created:', repo_dict['created_at'])
-print('Updated:', repo_dict['updated_at'])
-print('Description', repo_dict['description'])
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+  print('Name:', repo_dict['name'])
+  print('Owner:', repo_dict['owner']['login'])
+  print('Stars:', repo_dict['stargazers_count'])
+  print('Repository:', repo_dict['html_url'])
+  print('Description:', repo_dict['description'])
