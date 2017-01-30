60216e5df44d4b25f792ce4e4a1a632da9150943
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-30 17:06:00 -0600

    Plotting the Data

--- Working_With_APIs/python_repos.py
+++ Working_With_APIs/python_repos.py
@@ -13,11 +13,17 @@ print("Total repositories:", response_dict['total_count'])
 
 # explore information about the repositories
 repo_dicts = response_dict['items']
+print("Number of items:", len(repo_dicts))
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
   names.append(repo_dict['name'])
-  stars.append(repo_dict['stargazers_count'])
+
+  plot_dict = {
+    'value': repo_dict["stargazers_count"],
+    'label': repo_dict["description"],
+  }
+  plot_dicts.append(plot_dict)
 
 # make visualization
 my_style = LS('#333366', base_style=LCS)
@@ -36,5 +42,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = "Most-Starred Python Projects on GitHub"
 chart.x_labels = names
 
-chart.add("", stars)
+chart.add("", plot_dicts)
 chart.render_to_file('python_repos.svg')
