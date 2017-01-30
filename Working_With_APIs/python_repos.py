5abbd3c7b1e19a0548fbb2f799ebafb69039dd68
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-30 17:08:28 -0600

    Adding Clickable Links to Our Graph

--- Working_With_APIs/python_repos.py
+++ Working_With_APIs/python_repos.py
@@ -22,6 +22,7 @@ for repo_dict in repo_dicts:
   plot_dict = {
     'value': repo_dict["stargazers_count"],
     'label': repo_dict["description"],
+    'xlink': repo_dict["html_url"],
   }
   plot_dicts.append(plot_dict)
 
